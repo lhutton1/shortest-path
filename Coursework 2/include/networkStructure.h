6461efb4b065d50a274f0ea4eedbf912e24f0a24
lhutton1 <luke.hutton1@outlook.com>
2018-04-21 14:09:07 +0100

    added uthash disclaimer

--- Coursework 2/include/networkStructure.h
+++ Coursework 2/include/networkStructure.h
@@ -17,12 +17,34 @@ struct adjList {
   int id;                       // id of node
   double x;                     // x position of node on map
   double y;                     // y position of node on map
-  double distance;              // distance of node from start node
+  double distance;                // distance of node from start node
   bool path;                    // is node part of a path?
   struct adjList *parent;       // parent node - djikstras
   struct adjListNode *head;     // head of linked adjacency list
 
   UT_hash_handle hh; /* Make structure hashable */
+
+  /* To be able to use UT hash, this disclaimer must be included */
+  /*Copyright (c) 2005-2016, Troy D. Hanson  http://troydhanson.github.com/uthash/
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+  OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 };
 
 typedef struct adjList AdjList, *AdjListP;
@@ -34,5 +56,7 @@ struct network {
 };
 
 typedef struct network Network, *NetworkP;
+
+
 
 #endif
